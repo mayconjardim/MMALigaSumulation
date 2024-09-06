37a2da8b99bed551f889b21004523844e388c406
Maycon Jardim <mayconsmill@gmail.com>
2024-09-06 15:00:36 -0300

    Criando classe Fighter

---
+++ MMALigaSumulation.Shared/Fighters/Fighter.cs
@@ -0,0 +1,22 @@
+﻿namespace MMALigaSumulation.Shared.Fighters
+{
+    public class Fighter
+    {
+
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string NickName { get; set; } = String.Empty;
+        public string Country { get; set; } = String.Empty;
+        public DateTime BirthDate { get; set; } 
+        public int HeightFeet { get; set; }
+        public int HeightInches { get; set; }
+        public int WeightClassId { get; set; }
+        public double Weight { get; set; }
+        public double Reach { get; set; }
+        public int Version { get; set; }
+        public bool Retired { get; set; }
+        public int CareerStatus { get; set; }
+
+
+    }
+}
