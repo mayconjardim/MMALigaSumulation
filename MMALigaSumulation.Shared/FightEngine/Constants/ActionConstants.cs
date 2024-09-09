bba724abb1905006550370a0b28e23f51f5eb8aa
Maycon Jardim <mayconsmill@gmail.com>
2024-09-09 13:10:09 -0300

    Criando Constants de ação

---
+++ MMALigaSumulation.Shared/FightEngine/Constants/ActionConstants.cs
@@ -0,0 +1,73 @@
+﻿namespace MMALigaSumulation.Shared.FightEngine.Constants
+{
+    public static class ActionConstants
+    {
+
+        public const int Punches = 1;
+        public const int Kicks = 2;
+        public const int Clinch = 3;
+        public const int Takedowns = 4;
+        public const int DirtyBoxing = 5;
+        public const int ThaiClinchPunches = 6;
+        public const int TakedownClinch = 7;
+        public const int BreakClinch = 8;
+        public const int GnP = 9;
+        public const int Positioning = 10;
+        public const int Submission = 11;
+        public const int StandingSub = 12;
+        public const int Standup = 13;
+        public const int Lnp = 14;
+        public const int FancyPunch = 15;
+        public const int FancyKick = 16;
+        public const int Headbutt = 17;
+        public const int Bite = 18;
+        public const int Poke = 19;
+        public const int Rest = 20;
+        public const int GroinKick = 21;
+        public const int Slam = 22;
+        public const int Suplex = 23;
+        public const int SoccerKicks = 24;
+        public const int Stomps = 25;
+        public const int StandKick = 26;
+        public const int MoveToGround = 27;
+        public const int StrikesFromGuard = 28;
+        public const int GroundKick = 29;
+        public const int RestClinch = 30;
+        public const int NoAction = 31;
+        public const int AllowStand = 32;
+        public const int PunchExchange = 33;
+        public const int PullGuard = 34;
+        public const int GnPElbows = 35;
+        public const int CapitalizeStand = 36;
+        public const int CapitalizeGround = 37;
+        public const int KneesOnGround = 38;
+        public const int FancySub = 39;
+        public const int ThaiClinchKnees = 40;
+        public const int GrapplingKnee = 41;
+        public const int GrapplingPunch = 42;
+
+        // Quedas
+        public const int Judo = 1;
+        public const int Wrestling = 2;
+
+        // Clinch Ataque
+        public const int ThaiAttack = 1;
+        public const int DirtyBoxingAttack = 2;
+        public const int GrapplingAttack = 3;
+
+        // Gurdas
+        public const int RearMount = 0;
+        public const int FullMount = 1;
+        public const int SideMount = 2;
+        public const int HalfGuard = 3;
+        public const int OpenGuard = 4;
+        public const int ClosedGuard = 5;
+        public const int ButterflyGuard = 6;
+
+        // Clinch
+        public const int ClinchDirtyBoxing = 0;
+        public const int ThaiClinch = 1;
+        public const int SimpleGrappling = 2;
+    }
+
+}
