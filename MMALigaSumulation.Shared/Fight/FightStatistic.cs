2da8a4e22d2a206aa0923ab7e4a947d8f0710063
Maycon Jardim <mayconsmill@gmail.com>
2024-09-09 12:47:07 -0300

    Criando Classe FightStatistic

---
+++ MMALigaSumulation.Shared/Fight/FightStatistic.cs
@@ -0,0 +1,37 @@
+﻿namespace MMALigaSumulation.Shared.Fight
+{
+    public class FightStatistic
+    {
+
+        public int PunchesLaunched { get; set; }
+        public int PunchesLanded { get; set; }
+        public int KicksLaunched { get; set; }
+        public int KicksLanded { get; set; }
+        public int ClinchStrLaunches { get; set; }
+        public int ClinchStrLanded { get; set; }
+        public int SubmissionsAttemps { get; set; }
+        public int SubmissionsAchieved { get; set; }
+        public int GrapplingAttemps { get; set; }
+        public int GrapplingAchieved { get; set; }
+        public int GnPStrLaunched { get; set; }
+        public int GnPStrLanded { get; set; }
+        public int TimeOnTheGround { get; set; }
+        public int KnockDownTimes { get; set; }
+        public int KnockedDownOpponentTimes { get; set; }
+        public int TakedownsAttemps { get; set; }
+        public int TakedownsAchieved { get; set; }
+        public double DamageDone { get; set; }
+        public double DamageReceived { get; set; }
+        public int InisWon { get; set; }
+        public int PerfomanceBonus { get; set; }
+        public double DamageStanding { get; set; }
+        public double DamageClinch { get; set; }
+        public double DamageGround { get; set; }
+        public double DamageRStanding { get; set; }
+        public double DamageRClinch { get; set; }
+        public double DamageRGround { get; set; }
+        public double TempDamageGround { get; set; }
+        public double TempDamageClinch { get; set; }
+
+    }
+}
